9daf2f9bd3b3e78b10f7092d468d1dccebf10387
Dale Blackburn <dale@dakebl.co.uk>
2020-02-09 11:12:44 +0000

    :recycle: Simplified the starter to use a basic theme

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,14 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
 import React from "react";
-
-import Index from './pages/Index'
+import { Box, Heading } from '@theme-ui/components'
 
 export default () => {
   return (
-    <>
-      <Index />
-    </>
+    <Box variant="styles.root">
+      <Heading as="h1">
+        Hello World!
+      </Heading>
+    </Box>
   )
 }
--- src/theme.js
+++ src/theme.js
@@ -1,13 +1,30 @@
-// example theme.js
+const rangeOfThrees = [ 0, 3, 6, 12, 15, 18, 21, 24, 27, 30, 33, 36 ]
+
 export default {
   fonts: {
-    body: 'system-ui, sans-serif',
-    heading: '"Avenir Next", sans-serif',
-    monospace: 'Menlo, monospace',
+    body: '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"',
+    heading: 'inherit'
   },
+  fontWeights: {
+    body: 400,
+    heading: 700,
+  },
+  fontSizes: [
+    12, 14, 16, 20, 24, 32, 48, 64,
+  ],
   colors: {
-    text: '#000',
-    background: '#fff',
-    primary: '#33e',
+    light: '#fffefe',
+    dark: 'rgb(51, 51, 51)',
+    darkBlue: '#2f4146',
   },
+  radii: rangeOfThrees,
+  space: rangeOfThrees,
+  text: {},
+  styles: {
+    root: {
+      fontFamily: 'body',
+      height: '100vh',
+      p: 5
+    }
+  }
 }
